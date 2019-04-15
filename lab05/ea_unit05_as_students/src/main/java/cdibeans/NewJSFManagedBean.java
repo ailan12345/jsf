7d6a43f745935f11cbc35e8c0c1461aa429fd25d
ailan12345 <karta2599434@gmail.com>
2019-04-16 03:41:47 +0800

    lab 6

---
+++ lab05/ea_unit05_as_students/src/main/java/cdibeans/NewJSFManagedBean.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cdibeans;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author karta
+ */
+@Named(value = "newJSFManagedBean")
+@RequestScoped
+public class NewJSFManagedBean {
+
+    /**
+     * Creates a new instance of NewJSFManagedBean
+     */
+    public NewJSFManagedBean() {
+       
+    }
+    
+//    public String toPage1() {
+//        return "page1";
+//    }
+//    
+//    public String toPage2() {
+//        return "page2";
+//    }
+//    
+//    public String toPage3() {
+//        return "page3";
+//    }
+    
+}
