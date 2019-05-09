eca4e89fed19042fc27516329cc37259d507bd37
hychen39-T460S <hychen39@gmail.com>
2019-05-09 22:33:46 +0800

    Resolve issue #1. 	modified:   unit07/unit07/src/conf/persistence.xml 	modified:   unit07/unit07/src/java/ejb/ItemFacade.java

--- unit07/unit07/src/java/ejb/ItemFacade.java
+++ unit07/unit07/src/java/ejb/ItemFacade.java
@@ -10,6 +10,7 @@ import entityControl.AbstractFacade;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceUnit;
 
 /**
@@ -20,8 +21,15 @@ import javax.persistence.PersistenceUnit;
 @LocalBean
 public class ItemFacade extends AbstractFacade<Item>{
 
-    
-    @PersistenceUnit(unitName = "unit07PU")   
+    /* Note to the revision
+      @PersistenceUnit 和 PersistenceContext 是不同的, 應使用 @PersistenceContext. <br/>
+      @PersistenceUnit: Expresses a dependency on an EntityManagerFactory and its associated persistence unit. 
+    see: https://docs.oracle.com/javaee/6/api/javax/persistence/PersistenceUnit.html  <br/>
+    @PersistenceContext: Expresses a dependency on a container-managed EntityManager and its associated persistence context.
+    See: https://docs.oracle.com/javaee/7/api/javax/persistence/PersistenceContext.html
+        
+    */
+    @PersistenceContext(unitName = "unit07PU")   
     private EntityManager em;
         
     public ItemFacade() {
