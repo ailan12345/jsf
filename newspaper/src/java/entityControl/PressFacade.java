57750f74351f3f05636670ca1478e076cede73c3
ailan12345 <karta2599434@gmail.com>
2019-06-19 22:17:58 +0800

    add facade

---
+++ newspaper/src/java/entityControl/PressFacade.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entityControl;
+
+import entities.News;
+import entities.Press;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author ailan
+ */
+@Stateless
+@LocalBean
+public class PressFacade extends AbstractFacade<Press> {
+
+    @PersistenceContext(unitName = "newspaperPU")
+    private EntityManager em;
+    
+    // default constructor
+    public PressFacade() {
+        super(Press.class);
+    }
+    
+    
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+}
